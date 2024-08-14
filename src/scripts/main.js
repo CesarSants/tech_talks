895538f2f50a106bc726db4040e2e7be0793a319
CesarSants <cesarsantosplay4@gmail.com>
2024-08-13 23:55:02 -0300

    arrumando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,7 @@ const contaAsHoras = setInterval(function(){
     const minutoEmMs = 1000 * 60;
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor((diasAteOEvento % diaEmMs) / horaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
